a18febecf0d5a42b53e206d7741992454e4c23c6
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-11 19:23:25 +0530

    chsnges

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor() {
+    super();
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    this.relative = ["ajay", "booby", "camy", "dank"];
+  }
+  render() {
+    return (
+      <div id="main">
+        {this.relative.map((rel) => (
+          <p key={rel}>{rel}</p>
+        ))}
+        fjdishjsfio
+      </div>
+    );
+  }
 }
-
 
 export default App;
